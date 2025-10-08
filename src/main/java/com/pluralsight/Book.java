e98a7d72600dacca0764a6daaa69901ceef462aa
Norly Pierre <norlypierre@gmail.com>
2025-10-07 23:44:33 -0400

    Fixed Constructor, removed unnecessary Setters, added Methods & used toString to change how it is printed.

--- src/main/java/com/pluralsight/Book.java
+++ src/main/java/com/pluralsight/Book.java
@@ -7,44 +7,54 @@ public class Book {
     private String checkedOutTo;
     private boolean isCheckedOut;
 
+    public Book(int id, String isbn, String title) {
+        this.id = id;
+        this.isbn = isbn;
+        this.title = title;
+        this.checkedOutTo = "";
+        this.isCheckedOut = false;
+    }
 
     public int getId() {
         return id;
     }
-    public void setId(int id) {
-        this.id = id;
-    }
     public String getIsbn() {
         return isbn;
-    }
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
     }
     public String getTitle() {
         return title;
     }
-    public void setTitle(String title) {
-        this.title = title;
-    }
     public String getCheckedOutTo() {
         return checkedOutTo;
-    }
-    public void setCheckedOutTo(String checkedOutTo) {
-        this.checkedOutTo = checkedOutTo;
     }
     public boolean isCheckedOut() {
         return isCheckedOut;
     }
-    public void setCheckedOut(boolean checkedOut) {
-        isCheckedOut = checkedOut;
-    }
-    public void  checkOut(String name) {
 
+
+    public void checkOut(String name) {
+        if (!isCheckedOut) {
+            isCheckedOut = true;
+            checkedOutTo = name;
+            System.out.println(title + "has been checked out to" + name);
+        } else {
+            System.out.println(title + "already has been checked out");
+        }
+    }
+    public void checkIn() {
+        if (isCheckedOut) {
+            isCheckedOut = false;
+            checkedOutTo = "";
+            System.out.println(title + "has been checked in");
+        } else {
+            System.out.println(title + "has not been checked out");
+        }
+    }
+
+    public String toString(){
+        if (isCheckedOut)
+            return String.format("%d | %s | %s | Checked out to: %s", id, isbn, title, checkedOutTo);
+        else
+            return String.format("%d | %s | %s", id, isbn, title);
     }
 }
-
-
-
-
-
-
---
+++ src/main/java/com/pluralsight/LibaryApp.java
@@ -0,0 +1,8 @@
+package com.pluralsight;
+
+public class LibaryApp {
+    public static void main(String[] args) {
+
+
+    }
+}
