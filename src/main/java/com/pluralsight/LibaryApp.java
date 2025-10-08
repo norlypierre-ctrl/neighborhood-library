02668b1d5b9f9e4cf23c6357870cb00ffdf5802f
Norly Pierre <norlypierre@gmail.com>
2025-10-08 00:34:29 -0400

    Declared Scanner and Book array with encapsulation. Added methods for filling book registry and main menu. Created a loop for the menu till exited. used switch case to allow user to choose a number for the menu options.

--- src/main/java/com/pluralsight/LibaryApp.java
+++ src/main/java/com/pluralsight/LibaryApp.java
@@ -1,8 +1,43 @@
 package com.pluralsight;
 
-public class LibaryApp {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class LibaryApp {
+
+    private static Book[] catalog = new Book[20];
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        addBooks;
+        homeScreen;
+    }
+
+    private static void homeScreen(){
+        while (true){
+            System.out.println("\n  Book Store  ");
+            System.out.println("1.Show Available Books");
+            System.out.println("2.Show Checked out Books");
+            System.out.println("3. Exit");
+            System.out.print("Select an option: ");
+
+            String choose = scanner.nextLine();
+
+            switch (choose){
+                case "1":
+                    showAvailableBooks();
+                    break;
+                case "2":
+                    showCheckedOutBooks();
+                    break;
+                case "3":
+                    System.out.println("Goodbye");
+                    return;
+                default:
+                    System.out.println("Option Invalid!");
+            }
+
+
+        }
 
     }
 }
